d51584211ecd148c2e4a6262e71ac5656fb162a3
paulochiaradia <paulo.chiaradia@outlook.com>
2023-02-23 19:45:38 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	s3Bucket := os.Getenv("S3_BUCKET")
+	fmt.Println(s3Bucket)
+}
